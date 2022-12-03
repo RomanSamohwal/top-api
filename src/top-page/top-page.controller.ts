f3dc741f3c7a0ef97beacd19a0d6222d683520d9
Roman <rsamohwal@gmail.com>
2022-12-03 23:43:45 +0300

    Добавил mongo.config.ts

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from "@nestjs/common";
+import { TopPageModel } from "./top-page.model";
+import { TopPageDto } from "./dto/top-page.dto";
 
-@Controller('top-page')
-export class TopPageController {}
+@Controller("top-page")
+export class TopPageController {
+
+  @Post("create")
+  async create(@Body() dto: Omit<TopPageModel, "_id">) {
+
+  }
+
+  @Get(":id")
+  async get(@Param("id") id: string) {
+
+  }
+
+  @Delete(":id")
+  async delete(@Param("id") id: string) {
+
+  }
+
+  @Patch(":id")
+  async patch(@Param("id") id: string, @Body() dto: TopPageModel) {
+
+  }
+
+  @HttpCode(200)
+  @Post()
+  async find(@Body() dto: TopPageDto) {
+
+  }
+}
