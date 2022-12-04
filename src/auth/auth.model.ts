2576652f6a173e537e4d0b9520541bb360a2a508
Roman <rsamohwal@gmail.com>
2022-12-04 23:39:19 +0300

    Добавил product.model.ts

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,4 +1,11 @@
-export class AuthModel {
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { prop } from "@typegoose/typegoose";
+
+export interface AuthModel extends Base {}
+
+export class AuthModel extends TimeStamps {
+  @prop({ index: true })
   email: string;
+  @prop()
   passwordHash: string;
 }
